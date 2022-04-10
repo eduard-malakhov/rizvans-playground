14bf2242aa38782c2f34ceffb98e169e29349a63
Eduard Malakhov <eduard.malakhov@revjet.com>
2022-04-10 23:17:11 +0300

    newsletter update

--- packages/panakeia/source/components/newsletter/index.tsx
+++ packages/panakeia/source/components/newsletter/index.tsx
@@ -1,8 +1,24 @@
 import React, { FunctionComponent } from 'react';
+import './styles.less';
+import BackgroundImage from './assets/images/background.png';
+
 
 const Newsletter: FunctionComponent = () => {
     return (
-        <div>Newsletter</div>
+        <div className='newsletter'>
+            <img className='background' src={ BackgroundImage } />
+            <div className='content'> 
+                <h1>Newsletter</h1>
+                <p>
+                    Ut arcu arcu, faucibus eu risus blandit, pulvinar viverra justo. Phasellus arcu justo,<br />
+                    ullamcorper non diam sit amet, luctus condimentum metus
+                </p>
+                <form>
+                    <input type="email" placeholder="email adress"></input>
+                    <div className='sign-up' role="button">Sign up</div>
+                </form>
+            </div>
+        </div>
     )
 }
 
